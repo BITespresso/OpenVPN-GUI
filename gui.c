3b7eb42eb117c890187aff6a0c47eecae4b1d04e
Michael Berger <michael.berger@gmx.de>
2011-09-25 11:49:46 +0200

    changed my email address

--- gui.c
+++ gui.c
@@ -1,7 +1,7 @@
 /*
  *  OpenVPN-GUI -- A Windows GUI for OpenVPN.
  *
- *  Copyright (C) 2011 Michael Berger <micoder@users.sf.net>
+ *  Copyright (C) 2011 Michael Berger <michael.berger@gmx.de>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
